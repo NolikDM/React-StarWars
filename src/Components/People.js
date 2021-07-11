ba058f03bb7fd7003806d1472c9f9b3324c7b89f
NolikDM <56555674+NolikDM@users.noreply.github.com>
2021-07-12 02:39:30 +0700

    clean and restart

--- src/Components/People.js
+++ src/Components/People.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import { Card, Grid } from 'semantic-ui-react';
 
-function People() {
+function People({people}) {
 	return (
-		<>
+		<div>
 			<h1>People</h1>
-		</>
+			<ul>
+				{people.map(person => {
+					return <li key={person.name}>{person.name}</li>
+				})}
+			</ul>
+
+			{/*<Grid columns={3}>*/}
+			{/*	{data.map((people, i) => {*/}
+			{/*		return (*/}
+			{/*			<Grid.Column key={i}>*/}
+			{/*				<Card>*/}
+			{/*					<Card.Content>*/}
+			{/*						<Card.Header>*/}
+			{/*							{people.name}*/}
+			{/*						</Card.Header>*/}
+			{/*						<Card.Description>*/}
+			{/*							<strong>Homeworld</strong>*/}
+			{/*							<p>{people.homeworld}</p>*/}
+			{/*						</Card.Description>*/}
+			{/*					</Card.Content>*/}
+			{/*				</Card>*/}
+			{/*			</Grid.Column>*/}
+			{/*		)*/}
+			{/*	})}*/}
+		</div>
 	)
 }
 
